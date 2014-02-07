4058b7ec3f20f3d56b44b388316970c44ad338ad
Jayson Cofell <kerick@shiftedbit.net>
2014-02-07 12:18:06 -0600

    Add older scripts

--- zenossURL.py
+++ zenossURL.py
@@ -12,30 +12,18 @@ minagencygraph = 3
 # TODO
 #####################################################################
 
-#Save list of unique domains - DONE
-#Save list of dpNames w/ reference to domain - DONE
-#Create Graph - DONE
-#get device/template/graph for the domain in question - DONE
-#Build graph - DONE
-#Setup separate graphs for agencies - DONE
-#Set ports for JVMs
-#Set Regular Expression for testing
-
 #####################################################################
 # Imports
 #####################################################################
 
 from os import path
 import sys
-#import os.path
-#from stat import *
-#import ZenModel
-#from ZenModel import Device
 import Globals
 from Products.ZenUtils.ZenScriptBase import ZenScriptBase
 from transaction import commit
 from re import split,sub
 from Products import ZenModel
+import itertools
 
 #####################################################################
 # Zenoss Setup
@@ -47,30 +35,31 @@ dmd = ZenScriptBase(connect=True).dmd
 # Variables
 #####################################################################
 
-template_name = ''
+#template_name = ''
 dpNames = []
 GraphPoints = []
 agencies = []
 agencycount = {}
+urls = {}
+domains = {}
 
 #####################################################################
 # Functions
 #####################################################################
 
-# Since Python 2.4 doesn't have nice list sorting, we have our own compare
-def cmp1(a,b): return cmp(a[1],b[1])
-
 # Hacks up URLs and determines SSL
 # Accepts URL
 # returns: [bool ssl, domain, url, space delim url]
-def parseURL(url):
+def parseLine(line):
     secure = 0
-    if url[0].startswith('http'):
-        if url[0].startswith('https'):
+    if line[0].startswith('http'):
+        port = line[(len(line)-1)].split(';')[1]
+        text = line[(len(line)-1)].split(';')[2]
+        if line[0].startswith('https'):
             secure = 1
-        url.pop(0)
-        url.pop(0)
-        return [secure, url.pop(0), ''.join(["/%s" % (path.rstrip('\n')) for path in url]), ''.join(["%s " % (path.rstrip('\n')) for path in url])]
+        line.pop(0)
+        line.pop(0)
+        return (secure, line.pop(0), ''.join(["/%s" % (path.split(';')[0]) for path in line]), ''.join(["%s " % (path.split(';')[0]) for path in line]).rstrip(' '), port, text)
 
 # Creates or Finds Device
 # Accepts: dmd object, device name
@@ -95,17 +84,29 @@ def findTemplate(device, template_name):
 # Creates Datasources on templates for Graphs
 # Accepts: template object, bool ssl, context root, context root space delimited
 # Returns: datapoint name
-def createDataSources(template, secure, url, urlSpaces):
-    DS = template.manage_addRRDDataSource(urlSpaces, 'HttpMonitorDataSource.HttpMonitor')
+def createDataSources(template, secure, url, urlSpaces, port, text):
+#    DS = template.manage_addRRDDataSource(urlSpaces, 'HttpMonitorDataSource.HttpMonitor')
+    proto = "http://"
+    DS = template.manage_addRRDDataSource(urlSpaces, 'WebTxDataSource.WebTx')
     DS.enabled = 'true'
     DS.eventClass = '/Status/Web'
-    DS.url = url
+    #DS.url = url
+    #DS.commandTemplate = "go https://secure.intranetappstest.nd.gov/itd/onlineservices/scheddowntime/admin/login.htm\n find 'Login'"
+    findquery = ""
+    if text:
+        findquery = ''.join(["\nfind ", "'", text, "'"])
     if secure:
         DS.useSsl = 1
         DS.port = 443
-    DP = DS.manage_addRRDDataPoint('size')
+        proto = "https://"
+    if port:
+        DS.port = port
+    DS.commandTemplate = ''.join(["go ", proto, "${dev/id}", url, findquery])
+    DP =  DS.manage_addRRDDataPoint('available')
+    #DP = DS.manage_addRRDDataPoint('size')
     DP.rrdtype = 'GAUGE'
-    DP = DS.manage_addRRDDataPoint('time')
+    DP =  DS.manage_addRRDDataPoint('totalTime')
+    #DP = DS.manage_addRRDDataPoint('time')
     DP.rrdtype = 'GAUGE'
     return DP.name()
 
@@ -140,102 +141,115 @@ def renameGraphLegend(graphPoint):
 def uniq(seq):
     return {}.fromkeys(seq).keys()
 
+# Line Parser
+def lineParser(inputfile):
+    templates = []
+    urls = []
+    domains = []
+    for line in inputfile:
+        if (line[:1].isalpha() and not line.startswith('http')):
+            templates.append(line)
+            tempidx = (len(templates)-1)
+            urls.append('')
+            urls[tempidx] = []
+            domains.append('')
+            domains[tempidx] = []
+        elif (line[:1].isalpha() and line.startswith('http')):
+            parsedLine = parseLine(line.split('/'))
+            domains[tempidx].append(parsedLine[1])
+            urls[tempidx].append(parsedLine)
+    urls.sort
+    domains = [ uniq(domain) for domain in domains ]
+    print domains
+# Create the template dictionary for urls
+    return dict(itertools.izip(templates, urls[:])), dict(itertools.izip(templates, domains[:]))
+
+
+#####################################################################
+# FILE Handling
+#####################################################################
+
+urlfile = open('testURLs.txt')
+fileobjects = [ objects.rstrip('\n') for objects in urlfile ]
+urlfile.close()
+del urlfile
+
+#####################################################################
+# Parser
+#####################################################################
+
+urls,domains = lineParser(fileobjects)
+
 #####################################################################
 # URL Parsing
 #####################################################################
 
-# Pulls out the template name by checking for any line not starting with a symbol or URL
-template_name = [ url.rstrip('\n') for url in open('testURLs.txt') if (url[:1].isalpha() and not url.startswith('http')) ][0]
-# Splits each URL and passes through parseURL function
-parsedURL = [ parseURL(url.split('/')) for url in open('testURLs.txt') if (url[:1].isalpha() and url.startswith('http')) ]
 # Acquires unique domain names
-domains = uniq([ domain[1] for domain in parsedURL ])
+#domains = uniq([ domain[1] for domain in parsedURL ])
 
-if graphbyagency == 1:
+#if graphbyagency == 1:
 # Grab a unique list of agencies
-    agencies = uniq([url[3].split(' ')[0] for url in parsedURL ])
-#    agencycount = dict([(agency,0) for agency in agencies ])
+#    agencies = uniq([url[3].split(' ')[0] for url in parsedURL ])
 #    print agencies
 
 #####################################################################
 # Main
 #####################################################################
 
-#generators
-#device = (acquireDevice(object[0]) for object in parsedURL)
-#template = (findTemplate(dev,template_name) for dev in device)
-#DP = (zip(domains.index(url[0]), createDataSources(template, url[0], url[2], url[3])) for url in parsedURL)
-#if template.getRRDDataSources():
-#    datasourcepaths = [datasource.url for datasource in template.getRRDDataSources()]
-#graph = (findGraph(template, template_name) for j in xrange(len(parsedURL)))
-    
-#    if object[3] in datasourcepaths:
-#        continue
-for url in parsedURL:
-    device = acquireDevice(dmd, url[1])
-    template = findTemplate(device, template_name)
-    # Stores index of unique domain, datapoint, and agency for graph creation...Appends tuple
-    dpNames.append((domains.index(url[1]), createDataSources(template, url[0], url[2], url[3]), url[3].split(' ')[0]))
-#    if graphbyagency == 1:
-#        agencycount[url[3].split(' ')[0]] += 1
-#        [ findGraph(template, template_name, graphbyagency, agency) for agency in agencies ]
-#    else:
-#        findGraph(template, template_name, graphbyagency, '')
-
-# Sort our DataPoint list
-dpNames.sort(cmp1)
-#if graphbyagency == 1:
-#    tempAgencies = [ dpName[2] for dpName in dpNames ]
-#    agencyCounts = [ (ag, tempAgencies.count(ag) for ag in agencies ]
-#    del tempAgencies
-
-# Cycle through our domains and create the devices, templates, graphs, and graphpoints as necessary
-for domainNumber in xrange(len(domains)):
-    print domains[domainNumber]
-    device = acquireDevice(dmd, domains[domainNumber])
-    template = findTemplate(device, template_name)
+#for url in parsedURL:
+print urls
+for template_name in urls.iterkeys():
     if graphbyagency == 1:
-# All agencies in this domain
-        tempAgencies = [ dpName[2] for dpName in dpNames if dpName[0] == domainNumber ]
-# Count the agencies for this domain
-        agencycount = dict([ (ag, tempAgencies.count(ag)) for ag in agencies])
-#        print tempAgencies
-#        agencyCounts = [ (ag, tempAgencies.count(ag)) for ag in agencies ]
-        del tempAgencies
-        for agency in agencies:
-            GraphPoints = []
-# Grab datapoints filtered by agency and domain
-            dataPoints = [ dpName[1] for dpName in dpNames if dpName[0] == domainNumber and dpName[2] == agency]
-            if agencycount[agency] >= minagencygraph:
-#            if agencyCounts[agency] >= minagencygraph:
-                print agency
-                print dataPoints
-                graph = findGraph(template, template_name, graphbyagency, agency)
-                GraphPoints.append(graph.manage_addDataPointGraphPoints(dataPoints))
-                [ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
-                print GraphPoints
-                del GraphPoints
-#            elif agencyCounts[agency] > 0:
-            elif agencycount[agency] > 0:
-                print agency
-                print dataPoints
-# Assign to the default template
-                graph = findGraph(template, template_name, graphbyagency, '')
-                GraphPoints.append(graph.manage_addDataPointGraphPoints(dataPoints))
-                [ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
-                print GraphPoints
-                del GraphPoints
-# Need to add the counts in and finish
-    else:
-        dataPoints = [ dpName[1] for dpName in dpNames if dpName[0] == domainNumber ]
-        graph = findGraph(template, template_name, graphbyagency, agency)
-        GraphPoints.append(graph.manage_addDataPointGraphPoints(dataPoints))
-        [ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
-    
-
-
-#print GraphPoints
-# Rename the legend in the graph to something a little more human friendly
-#[ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
+         print "graphing"
+         #Grab a unique list of agencies
+         agencies = uniq([ url[3].split(' ')[0] for url in urls[template_name] ])
+    for url in urls[template_name]:
+        device = acquireDevice(dmd, url[1])
+        template = findTemplate(device, template_name)
+        # Stores index of unique domain, datapoint, and agency for graph creation...Appends tuple
+        dpNames.append((domains[template_name].index(url[1]), createDataSources(template, url[0], url[2], url[3], url[4], url[5]), url[3].split(' ')[0]))
+        del device
+        del template
+    # Sort our DataPoint list
+    dpNames.sort()
+    # Cycle through our domains and create the devices, templates, graphs, and graphpoints as necessary
+    for domainNumber in xrange(len(domains[template_name])):
+        print domains[template_name][domainNumber]
+        device = acquireDevice(dmd, domains[template_name][domainNumber])
+        template = findTemplate(device, template_name)
+        if graphbyagency == 1:
+            # All agencies in this domain
+            tempAgencies = [ dpName[2] for dpName in dpNames if dpName[0] == domainNumber ]
+            # Count the agencies for this domain
+            agencycount = dict([ (ag, tempAgencies.count(ag)) for ag in agencies])
+            #print tempAgencies
+            del tempAgencies
+            for agency in agencies:
+                GraphPoints = []
+                # Grab datapoints filtered by agency and domain
+                dataPoints = [ dpName[1] for dpName in dpNames if dpName[0] == domainNumber and dpName[2] == agency]
+                if agencycount[agency] >= minagencygraph:
+                    print agency
+                    print dataPoints
+                    graph = findGraph(template, template_name, graphbyagency, agency)
+                    GraphPoints.append(graph.manage_addDataPointGraphPoints(dataPoints))
+                    [ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
+                    print GraphPoints
+                    del GraphPoints
+                elif agencycount[agency] > 0:
+                    print agency
+                    print dataPoints
+                    # Assign to the default template
+                    graph = findGraph(template, template_name, graphbyagency, '')
+                    GraphPoints.append(graph.manage_addDataPointGraphPoints(dataPoints))
+                    [ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
+                    print GraphPoints
+                    del GraphPoints
+        # Need to add the counts in and finish
+        else:
+            dataPoints = [ dpName[1] for dpName in dpNames if dpName[0] == domainNumber ]
+            graph = findGraph(template, template_name, graphbyagency, agency)
+            GraphPoints.append(graph.manage_addDataPointGraphPoints(dataPoints))
+            [ renameGraphLegend(graphPoint) for graphPoint in GraphPoints[0] ]
 
 commit()
